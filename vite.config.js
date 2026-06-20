a9b140b748a5f1915f325a1d17ae9ff782a01b1a
Janis Zisler <janis.zisler@gmail.com>
2026-06-20 04:18:16 +0200

    feat: configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ihk-todo-project/',
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
